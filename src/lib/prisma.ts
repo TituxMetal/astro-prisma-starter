3ac830b0b16280ba24e5bdba44413e5bc32024eb
Titux Metal (DEV) <github@lgdweb.fr>
2024-10-16 18:09:57 +0200

    feat: add the prisma client using the libsql adapter to support Turso

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+
+// 1. Import libSQL and the Prisma libSQL driver adapter
+import { createClient } from '@libsql/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import path from 'node:path'
+
+const dbPath = path.join(process.cwd(), 'prisma', 'replica.db')
+
+// 2. Instantiate libSQL
+export const libsql = createClient({
+  url: `file://${dbPath}`,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+  syncUrl: process.env.TURSO_DATABASE_URL!,
+  syncInterval: 600
+})
+
+// 3. Instantiate the libSQL driver adapter
+const adapter = new PrismaLibSQL(libsql)
+// Pass the adapter option to the Prisma Client instance
+export const prismaClient = new PrismaClient({ adapter })
